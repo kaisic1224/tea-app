db2c5f73f3442b972ba7175614cf4dccb6272d5b
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-07-19 14:00:49 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+  images: {
+    domains: ["lh3.googleusercontent.com"]
+  }
+};
+
+module.exports = nextConfig;
